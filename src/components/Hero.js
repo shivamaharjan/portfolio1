e822146c9840f86477ca48e50b82a95568987af2
Shiva Maharjan <shivamaharjan@Shivas-MacBook-Air.local>
2024-01-30 13:31:59 +1100

    long way to go

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,33 +5,59 @@ import { FaLinkedin } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
 import "./hero.css"; // Import your CSS file for animations
+import { Link } from "react-router-dom";
 
 function Hero() {
+  const buttonStyle = {
+    fontSize: "10px",
+    backgroundColor: "#ecf0f1",
+    border: "1px solid ",
+    transition: "background-color 0.3s, border-color 0.3s",
+    borderColor: "white",
+    color: "black",
+  };
   return (
-    <div className="hero-container">
+    <div>
+    
+    <div className="hero-container d-flex juistify-content-center align-items-center">
       <Container>
-        <div className="hero-content">
+      <div className="d-flex justify-content-center align-items-center">
           <Image
             src={img1}
             alt="Hero Image"
             fluid
+            style={{
+              borderRadius: "50%",
+              maxHeight: "5rem",
+              marginRight: "2rem",
+            }}
             className="hero-image"
           />
+
           <div className="hero-text">
             <h1>Shiva Maharjan</h1>
-            <p>Front-End Developer</p>
+              <p>Front-End Developer</p>
           </div>
         </div>
-        <div className="hero-buttons">
-          <Button className="hero-button">Download Resume</Button>
-          <div className="hero-social-icons">
-            <FaLinkedin className="social-icon" />
-            <AiFillInstagram className="social-icon" />
-            <MdEmail className="social-icon" />
+        <div className="hero-buttons d-flex justify-content-center align-items-center hero-buttons">
+        <Button style={buttonStyle}
+        className="social-icon"
+          >
+            Download Resume
+          </Button>
+          <div className="hero-social-icons d-flex ms-4">
+            <Link to="/" className="nav-link"><FaLinkedin className="social-icon" /></Link>
+            <Link to="/" className="nav-link"><AiFillInstagram className="social-icon" /></Link>
+            <Link to="/" className="nav-link"><MdEmail className="social-icon" /></Link>
+            
           </div>
         </div>
+
       </Container>
+
     </div>
+    </div>
+    
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import Hamburger from "hamburger-react";
-import { Link as ScrollLink, Element } from "react-scroll";
+import { Link as ScrollLink} from "react-scroll";
+import "./Navbar.css";
+
 
 function NavBar() {
   const [isOpen, setOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset)
+  const [visible, setVisible] = useState(true);
   const isXlScreen = useMediaQuery({ query: "(min-width: 982px)" });
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isScrollingUp = prevScrollPos > currentScrollPos || currentScrollPos < 10;
+  
+      setPrevScrollPos(currentScrollPos);
+      setVisible(isScrollingUp);
+      
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    const navbar = document.getElementById("navbar");
+    if (navbar) {
+      navbar.style.transition = "top 0.3s";
+      navbar.style.top = visible ? "0" : `-${navbar.offsetHeight}px`;
+    }
+  }, [visible]);
+
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  }
+  
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
+
+
   return (
-    <div>
-      <Navbar expand="lg mb-5">
+    <div id="navbar" className="fixed-top" >
+      <Navbar expand="lg mb-5" style={{ zIndex: isOpen ? 1001 : 999 }}>
         <Container className="d-flex justify-content-between align-items-center">
           <Link to="/" className="nav-link">
             <h1>SM</h1>
@@ -34,14 +73,43 @@ function NavBar() {
                 </ScrollLink>
               </div>
             ) : (
-              // Render hamburger for smaller screens
+              <div>
               <div className="text-color">
-                <Hamburger toggled={isOpen} toggle={setOpen} size={30} />
+                <Hamburger className="hamburger-menu" toggled={isOpen} toggle={toggleMenu} size={30} />
+              </div>
               </div>
             )}
           </div>
         </Container>
       </Navbar>
+      {isOpen && (
+                <div className="mobile-menu-overlay" onClick={closeMenu}>
+                  <div className="mobile-menu" onClick={(e) => e.stopPropagation()}>
+                  <ul className="menu-list">
+                      <li onClick={closeMenu}>
+                        <ScrollLink to="about" smooth={true} duration={500}>
+                          About
+                        </ScrollLink>
+                      </li>
+                      <li onClick={closeMenu}>
+                        <ScrollLink to="project" smooth={true} duration={500}>
+                          Projects
+                        </ScrollLink>
+                      </li>
+                      <li onClick={closeMenu}>
+                        <ScrollLink to="experience" smooth={true} duration={500}>
+                          Experience
+                        </ScrollLink>
+                      </li>
+                      <li onClick={closeMenu}>
+                        <ScrollLink to="contact" smooth={true} duration={500}>
+                          Contacts
+                        </ScrollLink>
+                      </li>
+                    </ul>
+                    </div>
+                </div>
+              )}
     </div>
   );
 }
